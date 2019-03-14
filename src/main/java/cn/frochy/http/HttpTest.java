d43db8ce3cbe7edaf23077c347023f5edd8a138a
Frochy <ztyzqq@outlook.com>
2019-03-14 19:52:09 +0800

    bug fix

--- src/main/java/cn/frochy/http/HttpTest.java
+++ src/main/java/cn/frochy/http/HttpTest.java
@@ -1,19 +1,14 @@
 package cn.frochy.http;
 
-import cn.frochy.entity.SendMsgRes;
-import cn.frochy.xmlparse.SendMsgResParse;
-import org.junit.Test;
 
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class HttpTest {
 
     private static final String url = "http://10.250.6.86:8080/MessageTransferWebAppTL/servlet/messageTransferServiceServletByXml";
 
-    @Test
-    public void demo01() {
+    public static void main(String[] args) {
         Map<String, String> param = new HashMap<>();
         param.put("userName", "devtest");
         param.put("passWord", "888888");
@@ -22,11 +17,5 @@ public class HttpTest {
         param.put("body", "你好");
 
         System.out.println(HttpRequest.post(url,param));
-//        List<SendMsgRes> msgRess = SendMsgResParse.parseXmlByDoc(HttpRequest.post(url, param));
-//        if (msgRess != null && msgRess.size() > 0)
-//            for (SendMsgRes msgRes : msgRess)
-//                System.out.println(msgRes);
-//        else
-//            System.out.println("null");
     }
 }
